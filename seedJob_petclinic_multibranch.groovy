b2899da3e4b388698ed69777d994e785239f813e
Anqing Xu <axu@MacBook-Pro.local>
2020-11-09 21:53:03 -0500

    fix

--- seedJob_petclinic_multibranch.groovy
+++ seedJob_petclinic_multibranch.groovy
@@ -1,30 +1,15 @@
-//See https://gist.github.com/tknerr/c79a514db4bdbfb4956aaf0ee53836c8#file-ci_jobs-groovy
-// define the Git project + repos we want to build
-def git_project = 'anqingxu'
-def git_repos = ['spring-petclinic']
-
-// create a pipeline job for each of the repos and for each feature branch.
-for (git_repo in git_repos)
-{
-  multibranchPipelineJob("${git_repo}-ci") {
+multibranchPipelineJob('spring-petclinic-ci') {
     branchSources {
-      git {
-          id(${git_project} + "_" + ${git_repo}) // IMPORTANT: use a constant and unique identifier
-          remote('https://github.com/${git_project}/${git_repo}.git')
-          credentialsId('github_id');
-          includes('Jenkinsfile')
-      }
+        git {
+            id('123456789') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/anqingxu/spring-petclinic.git')
+            credentialsId('github-id')
+            includes('JENKINS-*')
+        }
     }
-
-    // check every minute for scm changes as well as new / deleted branches
-      triggers {
-        periodic(1)
-      }
-
     orphanedItemStrategy {
-      discardOldItems {
-          numToKeep(20)
-      }
+        discardOldItems {
+            numToKeep(20)
+        }
     }
-  }
 }
